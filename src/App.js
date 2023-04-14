094de6f21f95510f234e6253c792aa613c5653dc
unknown <marchioripedro1@gmail.com>
2023-04-14 01:53:38 -0300

    update all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(){
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <form onSubmit={presion}>
+        <p>Escriba:
+          <input type="letter" name="valor1" />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          <input type="submit" value="igresar" />
+        </p>
+      </form>
     </div>
   );
-}
 
+  function presion(e) {
+    e.preventDefault();
+   let palabra=(e.target.valor1.value);
+   
+   let vocales = ['a','e','i','o','u'];
+   let contador = 0;
+   
+   for(let i = 0; i < palabra.length; i++){
+       for(let l = 0; l < vocales.length; l++){
+          
+       if(palabra[i] == vocales[l]){contador++;}
+   
+       }
+   }
+   alert('Vocales en la oracion: ' +contador);
+   }
+}
+  
 export default App;
